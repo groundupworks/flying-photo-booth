ef9e96b82a552a757c549c67cfa32354bbdc50f1
Benedict Lau <ben.hy.lau@gmail.com>
2014-10-05 21:36:52 -0400

    Added yksp capture mode tests

---
+++ party-photo-booth/yksp-tests/scripts/CaptureModeAutomaticTest.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+import sys
+import os
+
+try:
+    sys.path.append(os.environ['YKSP_HOME'])
+except:
+    pass
+
+from yksptestcase import YkspTestCase
+
+NUM_CAPTURE_LOOPED = 5
+
+class CaptureModeAutomaticTestCase(YkspTestCase):
+
+    def testCaptureLooped(self):
+        '''
+        Tests the Automatic Mode capture sequence NUM_CAPTURE_LOOPED times.
+        '''
+        self.launchApp()
+        self.refreshScreen()
+
+        # Set up
+        self.vc.findViewWithTextOrRaise('Next').touch()
+        self.refreshScreen()
+        self.vc.findViewByIdOrRaise('id/no_id/11').touch()
+        self.refreshScreen()
+        self.vc.findViewWithTextOrRaise('Automatic').touch()
+        self.saveScreen('automatic-selection')
+        self.vc.findViewWithTextOrRaise('Next').touch()
+        self.refreshScreen()
+        self.vc.findViewWithTextOrRaise('Next').touch()
+        self.refreshScreen()
+        self.vc.findViewWithTextOrRaise('OK').touch()
+        self.refreshScreen()
+
+        # Run capture sequence NUM_CAPTURE_LOOPED times
+        for i in range(0, NUM_CAPTURE_LOOPED):
+            text = self.vc.findViewWithTextOrRaise('1 of 3')
+            self.vc.findViewWithAttributeOrRaise('class', 'android.widget.Button', root=text.getParent()).touch()
+            self.saveScreen('automatic-ps%s' % i, sleep=20)
+            self.vc.findViewWithTextOrRaise('Thank you for using\nParty PhotoBooth')
+            self.assertIsNone(self.vc.findViewWithText('Submit'))
+            self.refreshScreen(sleep=20)
+            self.refreshScreen()
+
+
+if __name__ == '__main__':
+    YkspTestCase.main(sys.argv)
---
+++ party-photo-booth/yksp-tests/scripts/CaptureModeSelfServeTest.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+import sys
+import os
+
+try:
+    sys.path.append(os.environ['YKSP_HOME'])
+except:
+    pass
+
+from yksptestcase import YkspTestCase
+
+NUM_CAPTURE_LOOPED = 5
+
+class CaptureModeSelfServeTestCase(YkspTestCase):
+
+    def testCaptureLooped(self):
+        '''
+        Tests the Self-serve Mode capture sequence NUM_CAPTURE_LOOPED times.
+        '''
+        self.launchApp()
+        self.refreshScreen()
+
+        # Set up
+        self.vc.findViewWithTextOrRaise('Next').touch()
+        self.refreshScreen()
+        self.vc.findViewByIdOrRaise('id/no_id/11').touch()
+        self.refreshScreen()
+        self.vc.findViewWithTextOrRaise('Self-serve').touch()
+        self.saveScreen('self-serve-selection')
+        self.vc.findViewWithTextOrRaise('Next').touch()
+        self.refreshScreen()
+        self.vc.findViewWithTextOrRaise('Next').touch()
+        self.refreshScreen()
+        self.vc.findViewWithTextOrRaise('OK').touch()
+        self.refreshScreen()
+
+        # Run capture sequence NUM_CAPTURE_LOOPED times
+        for i in range(0, NUM_CAPTURE_LOOPED):
+            text = self.vc.findViewWithTextOrRaise('1 of 3')
+            self.vc.findViewWithAttributeOrRaise('class', 'android.widget.Button', root=text.getParent()).touch()
+            self.refreshScreen(sleep=6)
+            text = self.vc.findViewWithTextOrRaise('2 of 3')
+            self.vc.findViewWithAttributeOrRaise('class', 'android.widget.Button', root=text.getParent()).touch()
+            self.refreshScreen(sleep=6)
+            text = self.vc.findViewWithTextOrRaise('3 of 3')
+            self.vc.findViewWithAttributeOrRaise('class', 'android.widget.Button', root=text.getParent()).touch()
+            self.saveScreen('self-serve-ps%s' % i, sleep=6)
+            self.vc.findViewWithTextOrRaise('Thank you for using\nParty PhotoBooth')
+            self.vc.findViewWithTextOrRaise('Submit').touch()
+            self.refreshScreen()
+
+
+if __name__ == '__main__':
+    YkspTestCase.main(sys.argv)
